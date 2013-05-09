7b94bd89e12d66d9a552563d0f86e833ecc26a88
Richard Wardin <shalmezad@gmail.com>
2013-05-09 09:37:49 -0400

    Test spins, works.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include "include/RouletteWheel.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout<<"Creating roulette wheel"<<endl;
+    RouletteWheel wheel;
+    cout<<"Spinning wheel 100 times:"<<endl;
+    for(int a=1; a<=100; a++)
+    {
+        RouletteResult spin = wheel.spin();
+        cout<<"Spin #"<<a<<": ";
+        cout<<"Number: ";
+        if(spin.number == -1){
+            cout<<"00";
+        }
+        else{
+            cout<<spin.number;
+        }
+        if(spin.color == RED){
+            cout<<" Color: Red";
+        }
+        if(spin.color == BLACK){
+            cout<<" Color: Black";
+        }
+        cout<<endl;
+    }
     return 0;
 }
